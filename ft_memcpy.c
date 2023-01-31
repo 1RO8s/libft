62da7d273b561fe509833070825878f695de1396
hnagasak <hnagasak@student.42tokyo.jp>
2023-01-31 21:27:47 +0900

    create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 21:16:57 by hnagasak          #+#    #+#             */
+/*   Updated: 2023/01/31 21:25:46 by hnagasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+
+    
+    return dst;
+}
+
+
+
+
+#include <string.h>
