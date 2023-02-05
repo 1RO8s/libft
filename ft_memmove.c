843ff28a16b16ff4a988ce2cf5b82515e96245d9
hnagasak <hnagasak@student.42tokyo.jp>
2023-02-05 20:07:47 +0900

    ft_memmove is being modified

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 17:16:17 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/02/05 17:49:54 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/02/05 18:32:51 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-    unsigned char *p = (unsigned char *)dst;
-    unsigned char *s = (unsigned char *)src;
+    unsigned    char    *p;
+    *p = (unsigned char *)dst;
+    unsigned char *s;
+    *s  = (unsigned char *)src;
     int n = len;
     while (n--)
     {
@@ -29,21 +31,21 @@ void *ft_memmove(void *dst, const void *src, size_t len)
     return dst;
 }
 
-#include <string.h>
-#include <stdio.h>
+// #include <string.h>
+// #include <stdio.h>
 
-int main(void)
-{
-    char dst1[10] = "aaaaaaaaa";
-    char src1[10] = "ABCDEFGHI";
+// int main(void)
+// {
+//     char dst1[10] = "aaaaaaaaa";
+//     char src1[10] = "ABCDEFGHI";
 
-    memmove(dst1,src1,5);
-    printf("dst1=%s\n",dst1);
+//     memmove(dst1,src1,5);
+//     printf("dst1=%s\n",dst1);
 
-    char dst2[10] = "BBBBBBBBB";
-    char src2[10] = "abcdefghi";
+//     char dst2[10] = "BBBBBBBBB";
+//     char src2[10] = "abcdefghi";
 
-    ft_memmove(dst2,src2,5);
-    printf("dst2=%s\n",dst2);
- }
+//     ft_memmove(dst2,src2,5);
+//     printf("dst2=%s\n",dst2);
+//  }
 
