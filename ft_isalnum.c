5d6aa1694c08ac9c6c9516fe3a74d9e8e36e4e50
hnagasak <hnagasak@student.42tokyo.jp>
2023-02-13 06:47:18 +0900

    include libft.h in ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,45 +6,13 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 23:30:00 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/01/14 23:47:36 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/02/13 06:45:06 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <ctype.h>
-
-int	ft_isupper(int c)
-{
-	return ('A' <= c && c <= 'Z');
-}
-
-int	ft_islower(int c)
-{
-	return ('a' <= c && c <= 'z');
-}
-
-int	ft_isalpha(int c)
-{
-	return (ft_isupper(c) || ft_islower(c));
-}
-
-int	ft_isdigit(int c)
-{
-	return ('0' <= c && c <= '9');
-}
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
-
-// int main(void)
-// {
-// 	int c;
-// 	c = '0';
-// 	printf("%d -> %d, %d\n",c ,ft_isalnum(c), isalnum(c));
-// 	c = 'a';
-// 	printf("%d -> %d, %d\n",c ,ft_isalnum(c), isalnum(c));
-// 	c = '-';
-// 	printf("%d -> %d, %d\n",c ,ft_isalnum(c), isalnum(c));
-// }
