1ff4a1f886bd0a0649cf7835df278e23852524b4
hnagasak <hnagasak@student.42tokyo.jp>
2023-02-13 09:12:41 +0900

    include libft.h in ft_memcpy, ft_toupper

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,45 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 21:16:57 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/01/31 21:40:40 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/02/13 09:10:19 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
-void *memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    unsigned char *p = dst;
-    while (n--)
-        *(p++) = (unsigned char)*(src++);
-    return dst;
+	unsigned char		*p;
+	const unsigned char	*s;
+
+	p = dst;
+	s = src;
+	while (n--)
+		*(p++) = *(s++);
+	return (dst);
 }
-
-// void *ft_memset(void *b, int c, size_t len)
-// {
-//     unsigned char *p = b;
-//     while (len--)
-//         *(p++) = (unsigned char)c;
-//     return b;    
-// }
-
-#include <string.h>
-#include <stdio.h>
-
-int main(void)
-{
-    char dst1[10] = "aaaaaaaaaa";
-    char src1[10] = "ABCDEFGHIJ";
-
-    memcpy(dst1,src1,5);
-    printf("dst=%s\n",dst1);
-
-    char dst2[10] = "aaaaaaaaaa";
-    char src2[10] = "ABCDEFGHIJ";
-
-    ft_memcpy(dst2,src2,5);
-    printf("dst=%s\n",dst2);
- }
--- ft_toupper.c
+++ ft_toupper.c
@@ -3,37 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 18:53:49 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/02/05 19:00:37 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/02/13 09:08:09 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_islower(int c)
+#include "libft.h"
+
+int	ft_toupper(int c)
 {
-	return ('a' <= c && c <= 'z');
+	if (ft_islower(c))
+	{
+		return (c - ('a' - 'A'));
+	}
+	return (c);
 }
-
-int ft_toupper(int c)
-{
-    if(ft_islower(c)){
-        return c - ('a' - 'A');
-    }    
-    return c;
-}
-
-// #include <ctype.h>
-// #include <stdio.h>
-
-// int main(void)
-// {
-//     printf("%d\n",toupper('a'));
-//     printf("%d\n",ft_toupper('a'));
-
-//     printf("%d\n",toupper('A'));
-//     printf("%d\n",ft_toupper('A'));
-
-//     printf("%d\n",toupper('-'));
-//     printf("%d\n",ft_toupper('-'));
-// }
