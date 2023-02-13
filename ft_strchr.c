28a63b072d976c8143e182009064bed73d688bc4
hnagasak <hnagasak@student.42tokyo.jp>
2023-02-13 09:15:25 +0900

    fixed norm error in ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,36 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 19:13:57 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/02/05 19:40:13 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/02/13 08:49:03 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    const char *result;
-    result = s;
-    while(*result){
-        //printf("%c,",*s);
-        if(*result == c){
-            //printf("%c!!,",*s);
-            return (char *)result;
-        }
-        result++;
-    }
-    //printf("\n%s,%d\n",s,c);
-    return NULL;
-}
+	const char	*result;
 
-#include <string.h>
-
-int main(void)
-{
-    char *str = "ABCDEFGHIJKLMN";
-    printf("%c(%p)\n",*strchr(str,'D'),strchr(str,'D'));
-    printf("%c(%p)\n",*ft_strchr(str,'D'),ft_strchr(str,'D'));
+	result = s;
+	while (*result)
+	{
+		if (*result == c)
+		{
+			return ((char *)result);
+		}
+		result++;
+	}
+	return (NULL);
 }
