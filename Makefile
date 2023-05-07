749c30bdfef291923ff8175dcc58f5dee5d8b22e
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-08 00:15:58 +0900

    add compile target

--- Makefile
+++ Makefile
@@ -2,7 +2,20 @@
 NAME	= libft.a
 
 # コンパイル対象ファイル。 \ で改行できる。
-SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_bzero.c \
+SRCS = \
+ft_isalpha.c\
+ft_isdigit.c\
+ft_isalnum.c\
+ft_isascii.c\
+ft_isprint.c\
+ft_strlen.c\
+ft_memset.c\
+ft_bzero.c\
+ft_memcpy.c\
+ft_memmove.c\
+ft_tolower.c\
+ft_toupper.c\
+ft_strlcat.c\
 main.c
 
 # 生成したいオブジェクト
--- libft.h
+++ libft.h
@@ -6,12 +6,32 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 06:14:48 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/02/13 06:41:16 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/02 17:55:57 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isupper(int c);
-int	ft_islower(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_isupper(int c);
+int		ft_islower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(\
+	char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlcat(\
+	char *restrict dst, const char *restrict src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+#endif
