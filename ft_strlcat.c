cef18ab50bbcd9887862a3e97e8f3b6a1fe08170
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-08 00:13:02 +0900

    create ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,16 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 18:44:38 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/02/05 18:44:45 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/07 18:57:49 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
-size_t strlcat(char * restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-    
+	char	*dst_end;
+	size_t	idx;
+	size_t	catlen;
+
+	catlen = ft_strlen(dst) + ft_strlen(src);
+	dst_end = dst;
+	while (*dst_end)
+	{
+		dst_end++;
+	}
+	idx = 0;
+	while (idx < dstsize && *src != '\0')
+	{
+		*dst_end = *src;
+		dst_end++;
+		src++;
+	}
+	return (catlen);
 }
