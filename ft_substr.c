9d7fbb365d10282cc04f3663a322a5e2bcb5e006
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-16 19:37:03 +0900

    fixed norm error

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:01:22 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/16 19:30:59 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:36:02 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *substr;
-	const char *sub_start;
+	char		*substr;
+	const char	*sub_start;
 
 	if (ft_strlen(s) + 1 < start)
 		return (NULL);
-
 	sub_start = s + start;
 	substr = ft_calloc(len + 1, sizeof(char));
 	if (substr != NULL)
 		ft_strlcpy(substr, (char *)sub_start, len + 1);
-
 	return ((char *)substr);
 }
