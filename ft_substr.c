8280490e0e6c11569db88c48e7a788990abc58d6
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-17 09:39:21 +0900

    fixed norm error

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,18 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:01:22 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/16 19:36:02 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/17 09:37:25 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char		*substr;
 	const char	*sub_start;
 
-	if (ft_strlen(s) + 1 < start)
+	if (ft_strlen(s) < start + 1)
 		return (NULL);
 	sub_start = s + start;
 	substr = ft_calloc(len + 1, sizeof(char));
