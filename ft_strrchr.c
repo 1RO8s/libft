f19d702ec91502ec973ce475b65a543924a2b9ce
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-19 14:28:53 +0900

    fixed ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 19:13:57 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/11 22:05:46 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/19 14:25:31 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	const char	*result;
+	char		ch;
 
+	ch = (char)c;
 	result = s;
 	while (*result)
-	{
 		result++;
-	}
 	while (s <= result)
 	{
-		if (*result == c)
-		{
+		if (*result == ch)
 			return ((char *)result);
-		}
 		result--;
 	}
 	return (NULL);
