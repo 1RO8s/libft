644a600432e8716db0a62fe61e080e2f1fcd2baa
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-20 03:14:42 +0900

    fixed ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 16:54:54 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/15 06:14:18 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/20 03:13:52 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,19 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	cmp_len;
 
 	if (*needle == '\0')
 		return ((char *)haystack);
+	if (len < ft_strlen(needle))
+		return (NULL);
+	cmp_len = ft_strlen(needle);
 	i = 0;
-	while ((char *)(haystack + i) && i + 1 < len)
+	while (haystack[i] != '\0' && i < len)
 	{
-		if (ft_strncmp((char *)(haystack + i), needle, ft_strlen(needle)) == 0)
+		if (len - i < cmp_len)
+			return (NULL);
+		if (ft_strncmp(&haystack[i], needle, cmp_len) == 0)
 			return ((char *)(haystack + i));
 		i++;
 	}
