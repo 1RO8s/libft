860cad24bf17ade8b609c86ede9b491236433c34
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-19 14:38:02 +0900

    fixed ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 19:13:57 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/19 14:36:28 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/19 14:37:28 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	const char		*result;
-	char	ch;
+	const char	*result;
+	char		ch;
 
 	ch = (char)c;
 	result = s;
