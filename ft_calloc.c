595761c443374c0635d0ebbf3f4cab60eae31873
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-20 05:51:27 +0900

    fixed ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 00:06:12 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/16 00:57:07 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/20 05:49:56 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
+	if (size != 0 && count > SIZE_MAX / size)
+		return (NULL);
 	mem = malloc(size * count);
 	if (mem != NULL)
 		ft_memset(mem, 0, size * count);
