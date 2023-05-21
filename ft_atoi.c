a43087b6e70d3fe28371dc059191d4768089ae2a
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-21 16:42:51 +0900

    fixed ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,43 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 06:23:58 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/16 10:52:49 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/21 16:42:23 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+int	is_space(char c)
+{
+	if (('\t' <= c && c <= '\r') || c == ' ')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *str)
 {
 	int		sign;
 	long	nbr;
 
+	sign = 1;
 	nbr = 0;
-	if (*str == '-')
+	while (is_space(*str))
+		str++;
+	if (*str == '-' || *str == '+')
 	{
-		sign = -1;
+		if (*str == '-')
+			sign = -1;
 		str++;
 	}
-	else
-		sign = 1;
-	while (*str != '\0')
+	while ('0' <= *str && *str <= '9')
 	{
-		if (!('0' <= *str && *str <= '9'))
-			break ;
+		if (LONG_MAX / 10 <= sign * nbr && LONG_MAX % 10 < (*str - '0'))
+			return (sign * (int)nbr);
+		if (LONG_MIN / 10 >= sign * nbr && -1 * (LONG_MIN % 10) < (*str - '0'))
+			return ((int)LONG_MIN);
 		nbr *= 10;
 		nbr += (*str - '0');
-		if (!(-2147483648 < (sign * nbr) && (sign * nbr) < 2147483647))
-			return (-1);
 		str++;
 	}
 	return (sign * (int)nbr);
