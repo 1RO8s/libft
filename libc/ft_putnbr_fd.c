f82f4f83793e57bbd36752e675f539e49ac7b865
hnagasak <hnagasak@student.42tokyo.jp>
2023-11-04 00:56:09 +0900

    fixed memory leak in ft_putnbr_fd.c.

--- libc/ft_putnbr_fd.c
+++ libc/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 04:05:08 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/19 04:07:55 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/11/03 21:09:52 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char *str;
+	str = ft_itoa(n);
+	ft_putstr_fd(str, fd);
+	free(str);
 }
